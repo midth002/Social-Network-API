5541007a62693adf75f922f73dc5920759dcf499
Andrew Midthun <midth002@gmail.com>
2022-05-15 17:59:31 -0500

    Can delete friends with the remove Friend method.

--- controllers/friendController.js
+++ controllers/friendController.js
@@ -9,5 +9,13 @@ module.exports = {
         )
         .then((friend) => res.json(friend))
         .catch((e) => res.status(500).json(e))  
+    }, 
+    removeFriend(req, res) {
+        User.findOneAndRemove({_id : req.params.friendId})
+        .then((friend) => 
+        !friend
+        ? res.status(404).json({ message : 'That is not a friend of yours with that ID.'})
+        : res.status(200).json({ message: 'Friend has been removed'}))
+        .catch((e) => res.status(500).json(e))
     }
 }
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -8,13 +8,14 @@ const {
 } = require('../../controllers/userController');
 
 const {
-    addFriend
+    addFriend,
+    removeFriend
 } = require('../../controllers/friendController')
 
 router.route('/').get(getUsers).post(createUser); 
 
 router.route('/:id').get(getOneUser).put(updateUser).delete(deleteUser);
 
-router.route('/:userId/friend/:friendId').put(addFriend)
+router.route('/:userId/friend/:friendId').put(addFriend).delete(removeFriend)
 
 module.exports = router;
